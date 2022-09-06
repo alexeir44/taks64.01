6419cbc9fbf864c7356eaedd75fb70ad132c4154
alexeir44 <110299540+alexeir44@users.noreply.github.com>
2022-09-06 16:21:15 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Enter the number n: ");
+int number = int.Parse(Console.ReadLine() ?? "");
+int start = 1;
+PrintNumber(number,start);
+
+void PrintNumber(int n, int start)
+{
+    if (start == n+ 1) return;
+    if (start == n)
+    {
+        Console.Write(start + ".");
+    }
+    else
+        Console.Write(start + ",");
+
+PrintNumber(n, start + 1);
+}
+    
+
+
